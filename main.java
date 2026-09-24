986af1300cb611c9008ef72808563f37e7fc0fa6
Fenherr <61088316+Fenherr@users.noreply.github.com>
2026-09-24 21:56:55 +0200

    variables string, tab and matrix

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+
+        String language = "Java";
+        int[] numbers = {1, 2, 3, 4, 5};
+        int[][] matrix = {{1, 2}, {3, 4}};
+
+        System.out.println("I am learning " + language);
+        System.out.println("Numbers: " + java.util.Arrays.toString(numbers));
+        System.out.println("Matrix: " + java.util.Arrays.deepToString(matrix));
+
+    }
+}
